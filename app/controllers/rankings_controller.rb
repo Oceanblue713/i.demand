ad04d2e00365c64b6ccc3a252dfdd6b1c0023399
Andy <amedamachip713@gmail.com>
2017-04-30 11:24:38 +0000

    test

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,6 @@
 class RankingsController < ApplicationController
   def index
-    @votes = Vote.find(Vote.group(:category).order('count(category) desc').pluck(:id))
-    @rankings = Vote.group(:category).order('count(category) desc').count(:category)
+    #@votes = Vote.find(Vote.group(:category).order('count(category) desc').pluck(:id))
+    @rankings = Vote.group(:category).order('count_category DESC').count(:category)
   end
 end
